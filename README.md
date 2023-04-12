ddb491ee03e4331f5dac5574d7078069e31b3ee7
Puripant R <puripantr@gmail.com>
2023-04-12 22:55:59 +0700

    Add voronoi diagram

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # คุณอยู่ตรงไหน Political Spectrum Poll
 
-ความเชื่อทางการเมืองของคุณอยู่ตรงไหน? ใกล้พรรคอะไร? โดยใช้ข้อมูลจาก พรรคนี้อยู่ตรงไหน? ในรายการ [พรรคนี้เป็นไงบ้าง?](https://www.youtube.com/playlist?list=PLg523oxEzw_ZgLDw6TyuVGaydyC1uMOA8) โดย [Farose](https://www.youtube.com/@FAROSECHANNEL)
+ความเชื่อทางการเมืองของคุณอยู่ตรงไหน? ใกล้พรรคอะไร? โดยใช้ข้อมูลจาก พรรคนี้อยู่ตรงไหน? ในรายการ [พรรคนี้เป็นไงบ้าง?](https://www.youtube.com/playlist?list=PLg523oxEzw_ZgLDw6TyuVGaydyC1uMOA8) โดย [FAROSE](https://www.youtube.com/@FAROSECHANNEL)
 
 ## TODO
 
--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-const width = 400;
-const height = 300;
+const width = 360;
+const height = 270;
+
+const blue_yellow = [
+  { offset: "0%", color: "blue" },
+  { offset: "50%", color: "blue" },
+  { offset: "50%", color: "gold" },
+  { offset: "100%", color: "gold" }
+]
+const blue_yellow_gradient = [
+  { offset: "0%", color: "blue" },
+  { offset: "100%", color: "gold" }
+]
+function define_gradient(defs, data, name) {
+  defs.append("linearGradient")
+      .attr("id", name)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", 1)
+      .attr("y2", 1)
+    .selectAll("stop")
+    .data(data)
+      .join("stop")
+      .attr("offset", d => d.offset)
+      .attr("stop-color", d => d.color);
+}
 
 let axis = d3.select("#axis")
   .append("svg")
@@ -23,17 +47,10 @@ axis.append("line")
   .attr("y2", height)
 
 const data = [
-  { "x":  0   , "y":  0   , "name": "ภูมิใจไทย", "color": "blue" },
-  { "x": -0.3 , "y":  0   , "name": "ชาติพัฒนากล้า", "color": "url(#blue-yellow)" },
-  { "x": -0.6 , "y":  0.5 , "name": "เพื่อไทย", "color": "red" },
-  { "x": -0.55, "y": -0.15, "name": "ก้าวไกล", "color": "darkorange" }
-]
-
-const blue_yellow = [
-  { offset: "0%", color: "blue" },
-  { offset: "50%", color: "blue" },
-  { offset: "50%", color: "gold" },
-  { offset: "100%", color: "gold" },
+  { "x":  0   , "y":  0   , "name": "พรรคภูมิใจไทย", "color": "blue", "color-voronoi": "blue" },
+  { "x": -0.3 , "y":  0   , "name": "พรรคชาติพัฒนากล้า", "color": "url(#blue-yellow)", "color-voronoi": "url(#blue-yellow-gradient)" },
+  { "x": -0.6 , "y":  0.5 , "name": "พรรคเพื่อไทย", "color": "red", "color-voronoi": "red" },
+  { "x": -0.55, "y": -0.15, "name": "พรรคก้าวไกล", "color": "darkorange", "color-voronoi": "darkorange" }
 ]
 
 let xScale = d3.scaleLinear()
@@ -49,18 +66,25 @@ let chart = d3.select("#chart")
   .attr("height", height)
   .attr("viewBox", [0, 0, width, height]);
 
-chart.append("defs")
-  .append("linearGradient")
-    .attr("id", "blue-yellow")
-    .attr("x1", 0)
-    .attr("y1", 0)
-    .attr("x2", 1)
-    .attr("y2", 1)
-  .selectAll("stop")
-  .data(blue_yellow)
-    .join("stop")
-    .attr("offset", d => d.offset)
-    .attr("stop-color", d => d.color);
+let defs = chart.append("defs")
+define_gradient(defs, blue_yellow, "blue-yellow");
+define_gradient(defs, blue_yellow_gradient, "blue-yellow-gradient");
+
+let delaunay = d3.Delaunay.from(data.map(d => [xScale(d.x), yScale(d.y)]));
+let voronoi = delaunay.voronoi([0, 0, width, height]);
+let cells = data.map((d, i) => [d, voronoi.cellPolygon(i)]);
+
+chart.append("g")
+    .attr("stroke", "white")
+    .attr("stroke-width", 1)
+    .attr("fill-opacity", 0.2)
+  .selectAll("path")
+  .data(cells)
+  .join("path")
+    .attr("d", ([_, cell]) => `${cell.map((p, i) => ((i === 0) ? "M" : "L") + p)}`)
+    .attr("fill", ([d, _]) => d["color-voronoi"])
+    .append("title")
+      .text(([d, _]) => `ใกล้เคียง${d.name}`);
 
 chart.append("g")
     .attr("stroke", "white")
